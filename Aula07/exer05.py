1496db5ff7de5091d959165dd86f94f98dbe316d
LuannMateus <luanmateuslima34@gmail.com>
2022-06-11 18:58:51 -0300

    feat: add Aula07

---
+++ Aula07/exer05.py
@@ -0,0 +1,8 @@
+def contador(tupla, x):
+    total = tupla.count(x)
+
+    print(total)
+
+tupla = (1, 2, 10, 20, 200, 200, -20)
+
+contador(tupla, 200)
---
+++ Aula07/exer06.py
@@ -0,0 +1,7 @@
+times = ('Flamengo', 'Santos', 'Palmeiras', 'Grêmio', 'Athletico-PR')
+
+print(times[0:3])
+
+print(times[-1])
+
+print(times.index('Santos'))
